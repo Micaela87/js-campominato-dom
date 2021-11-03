f6ae236d40dedd74104ffcfeb86e9eef94ffd53a
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-03 21:42:35 +0100

    issue on endGame being called more than once fixed

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -23,18 +23,22 @@ Al termine della partita il software deve scoprire tutte le bombe e comunicare i
 */
 
 // DOM elements
-const buttonContainer = document.getElementById('buttons');
-const gameContainer = document.getElementById('game');
-const squareContainer = document.querySelector('.container-square');
-const levelButtons = document.querySelectorAll('button');
+const buttonContainer = document.getElementById('buttons'); // interface with buttons for levels
+const gameContainer = document.getElementById('game'); // lets display the game itself when a button is clicked
+const squareContainer = document.querySelector('.container-square'); // contains the grid generated with generateSquares()
+const levelButtons = document.querySelectorAll('button'); // buttons for levels
+const finalResultContainer = document.getElementById('result'); // div to display the final result
 
 // global variables
-let newSquares = [];
-let onlyBombs = [];
-let arrBombs;
+let newSquares = []; // general array for all newDivs
+let onlyBombs = []; // array for newDivs with bombs
+let arrBombs; // stores 16 random numbers
+let hasLost = false; // flagging variable for bombs clicked
+let counter = 0; // keeps track of the click events
+let level; // store the value of the level button clicked
+let flaggingVariable = false; // prevents endGame() to be called more than once
 
 // generates squares according to the value of the button clicked
-// adds click event to each square generated
 const generateSquares = (number) => {
     arrBombs = generateBombs(number);
     console.log('arrBombs in function', arrBombs);
@@ -46,6 +50,7 @@ const generateSquares = (number) => {
         newDiv.append(newSpan);
         newDiv.classList.add('square');
         
+        // adds classes according to the number of squares to generate, so that the grid is always a square
         if (number === 81) {
             newDiv.classList.add('square-nine');
         } else if (number === 49) {
@@ -55,6 +60,7 @@ const generateSquares = (number) => {
         // checks if newDiv has bomb
         if (arrBombs.includes(i)) {
             onlyBombs.push(newDiv);
+            // newDiv.append('bomb');
             newSquares.push(newDiv);
         } else {
             newSquares.push(newDiv);
@@ -63,7 +69,8 @@ const generateSquares = (number) => {
         squareContainer.append(newDiv);
     }
 
-    handleClickEvent(number);
+    // adds click event on each newDiv previously generated
+    handleClickEvent();
 }
 
 // generates bombs
@@ -75,69 +82,66 @@ const generateBombs = (range) => {
             arrBombs.push(bomb);
         }
     }
-    console.log(arrBombs);
+    // console.log(arrBombs);
     return arrBombs;
 }
 
 // handles click event on squares
-const handleClickEvent = (number) => {
-    let limit = (number - arrBombs.length);
-    console.log(limit);
-    let counter = 32;
+const handleClickEvent = () => {
     newSquares.forEach(function(n) {
         n.addEventListener('click', function() {
-                if (counter < limit) {
-                    showBombs(n, counter, limit);
-                    counter++;
-                    console.log(counter)
-                } else {
-                    endGame(counter, limit);
-                }
-            }
-        );
+                    showBombs(n);
+            }, true);
     });
 }
 
-let hasLost = false;
+// shows bombs or clicked cells
+const showBombs = (element) => {
 
-// shows bombs
-const showBombs = (element, counter, limit) => {
-    if (onlyBombs.includes(element)) {
+    // the max number of click on different cells before winning
+    let limit = level - arrBombs.length;
+
+    if (onlyBombs.includes(element) && !flaggingVariable) {
         onlyBombs.forEach(function(x) {
             x.classList.add('bomb');
             x.children[0].classList.remove('hidden');
             hasLost = true;
-            endGame(counter, limit);
         });
-    } else if (!element.classList.contains('clicked-true')) {
+    }
+    
+    if (!onlyBombs.includes(element) && !element.classList.contains('clicked-true') && !hasLost) {
         element.classList.add('clicked-true');
         element.children[0].classList.remove('hidden');
+        counter++;
+    }
+
+    if ((hasLost || counter === limit) && !flaggingVariable) {
+        console.log('hasLost before calling endGame', hasLost)
+        endGame();
+        flaggingVariable = true;
     }
 }
 
-// // ends game
-const endGame = (counter, limit) => {
-    const result = document.createElement('div');
-    newSquares.forEach(function(n) {
-        n.removeEventListener('click', function() {
-            showBombs(n);
-        })
-    })
-    if ((counter === limit) && !hasLost) {
-        console.log('hai vinto')
-        // result.append(`Fantastico, hai vinto!`)
-    } else if (hasLost && (counter < limit)) {
-        // result.append(`hai fatto ${counter} tentativi, ma purtroppo hai perso`)
-        console.log(`hai fatto ${counter} tentativi, ma purtroppo hai perso`)
+// ends game
+const endGame = () => {
+    const winner = document.createElement('div');
+
+    if (hasLost) {
+        winner.append(`Hai perso! Hai totalizzato ${counter} punti prima di trovare la bomba`);
+    } else {
+        winner.append(`Complimenti, hai vinto! Hai totalizzato ${counter} punti`);
     }
 
-    document.body.append(result);
+    finalResultContainer.append(winner);
+    finalResultContainer.classList.remove('hidden');
+
 }
 
 // adds click event to each button
 levelButtons.forEach(function(n) {
     n.addEventListener('click', 
         function() {
+            level = Number(n.value);
             generateSquares(Number(n.value));
             gameContainer.classList.remove('hidden');
             buttonContainer.classList.add('hidden');
